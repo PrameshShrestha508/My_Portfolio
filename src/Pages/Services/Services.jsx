1d71571b341af61ae97c65305ad5abe495b82db6
pramesh shrestha <prameshshrestha455@gmail.com>
2022-04-18 11:34:29 +0545

    added services  Pages

---
+++ src/Pages/Services/Services.jsx
@@ -0,0 +1,89 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../../Component/Card/Card";
+import front from "../../Assets/Images/front.png";
+import backend from "../../Assets/Images/backend.png";
+import QA from "../../Assets/Images/QA.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from "./resume.pdf";
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Services</span>
+        <span></span>
+        <span>
+          Reliable Intern studying BSC CSIT doing an internship in Optimum
+          Futurist
+          <br />
+          with the aim of expanding skills and gain valuable real-world
+          experience
+        </span>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            image={front}
+            heading={"Frontend"}
+            detail={"HTML ,CSS, React  "}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            image={backend}
+            heading={"Backend"}
+            detail={"PHP, Laravel, NodeJs"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            image={QA}
+            heading={"QA"}
+            detail={"Use different QA tools"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
