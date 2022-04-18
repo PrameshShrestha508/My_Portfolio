dd1c713a1094486860eacd95766ca88b5c2badcf
pramesh shrestha <prameshshrestha455@gmail.com>
2022-04-18 11:23:39 +0545

    added Navbr

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
+  return (
+    <div className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
+        <div className="n-name">
+          <p>PBS</p>
+        </div>
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Serivces
+              </Link>
+            </li>
+
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="Blog" spy={true} smooth={true}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" spy={true} smooth={true}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
